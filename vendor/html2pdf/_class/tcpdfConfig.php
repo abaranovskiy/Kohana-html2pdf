d2da7c7162f23fcbf59959bfcadee773f8a7a3d7
abaranovskiy <pn.spell@gmail.com>
2013-12-09 10:53:06 +0300

    Update tcpdfConfig.php
    Fix undefined error

--- vendor/html2pdf/_class/tcpdfConfig.php
+++ vendor/html2pdf/_class/tcpdfConfig.php
@@ -62,6 +62,7 @@ if (!defined('K_TCPDF_EXTERNAL_CONFIG')) {
     $kPathMain.= '_tcpdf_'.HTML2PDF_USED_TCPDF_VERSION.'/';
     define('K_PATH_MAIN', $kPathMain);
 
+    $kPathUrl = '';
     // Automatic calculation for the following K_PATH_URL constant
     if (isset($_SERVER['HTTP_HOST']) AND (!empty($_SERVER['HTTP_HOST']))) {
         if (isset($_SERVER['HTTPS']) AND (!empty($_SERVER['HTTPS'])) AND strtolower($_SERVER['HTTPS'])!='off') {
